0fb2f3cf70fabc80530f8fcc475543ce74c6f8bc
zhangyibo1012 <35024391+zhangyibo1012@users.noreply.github.com>
2019-08-06 23:14:14 +0800

    初始化项目

---
+++ farm-upms/src/main/java/cn/orgtec/farm/FarmApplication.java
@@ -0,0 +1,18 @@
+package cn.orgtec.farm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 农家乐管理系统
+ *
+ * @author Yibo Zhang
+ * @date 2019/08/06
+ */
+@SpringBootApplication
+public class FarmApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FarmApplication.class, args);
+	}
+}
---
+++ farm-upms/src/main/java/cn/orgtec/farm/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Yibo Zhang
+ * @date 2019/08/06
+ */
+package cn.orgtec.farm.service;
